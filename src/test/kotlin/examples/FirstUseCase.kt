f0a1066409d7c78c6801f988d32c1d2cfd0146a9
Aleksander Vorobiev <avorobiev@cybertonica.com>
2021-01-04 13:27:42 +0300

    expanded SecondUseCase

--- src/test/kotlin/examples/FirstUseCase.kt
+++ src/test/kotlin/examples/FirstUseCase.kt
@@ -13,7 +13,7 @@ class FirstUseCase {
         val banknote500 = Banknote.init(bpk=BPK, bin=BIN, amount=500)
         assertTrue(banknote500.verify(bok=BOK))
 
-        val banknote100 = Banknote.init(bpk=BPK, bin=BIN, amount=500)
+        val banknote100 = Banknote.init(bpk=BPK, bin=BIN, amount=100)
         assertTrue(banknote100.verify(bok=BOK))
     }
 }
--- src/test/kotlin/examples/SecondUseCase.kt
+++ src/test/kotlin/examples/SecondUseCase.kt
@@ -7,11 +7,13 @@ import org.junit.Test
 import java.security.PublicKey
 
 class SecondUseCase {
-    private val walletAlice = Wallet(spk = A_SPK, sok = A_SOK, sokSignature = sok_signature(A_SOK))
-    val walletBob = Wallet(spk = B_SPK, sok = B_SOK, sokSignature = sok_signature(B_SOK))
-
     private val banknote500 = Banknote.init(bpk = BPK, bin = BIN, amount = 500)
-    val banknote100 = Banknote.init(bpk = BPK, bin = BIN, amount = 500)
+
+    private val walletAlice = Wallet(spk = A_SPK, sok = A_SOK, sokSignature = sok_signature(A_SOK))
+    private val walletBob = Wallet(spk = B_SPK, sok = B_SOK, sokSignature = sok_signature(B_SOK))
+
+    private lateinit var aliceInitSignature: String
+    private val None = null
 
     @Test
     fun useCase() {
@@ -19,6 +21,9 @@ class SecondUseCase {
 
         val block1 = bankToAlice()
         blockchain.add(block1)
+
+        val block2 = aliceToBob(block1)
+        blockchain.add(block2)
     }
 
     private fun bankToAlice(): OneBlock {
@@ -33,6 +38,7 @@ class SecondUseCase {
         // -------- Сторона: Алиса ----------------
         // Алиса создаёт новый блок
         val (uuid, _, otok, _, initWalletSignature) = walletAlice.newBlockParams(bnid = bnid1)
+        aliceInitSignature = initWalletSignature
 
         // ------- Канал связи --------------------
         // Передача данных от Алисы Банку: uuid, otok, initWalletSignature, sok, sokSignature
@@ -70,6 +76,80 @@ class SecondUseCase {
             magic = magic,
             subscribeTransactionHash = subscribeTransactionHash,
             subscribeTransactionSignature = subscribeTransactionSignature,
+        )
+    }
+
+    private fun aliceToBob(prevBlock: OneBlock): OneBlock {
+        // -----------------------------------------------------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------
+        // Второй блок
+        // Передача купюры: Алиса -> Боб
+        // -----------------------------------------------------------------------------------------------------------------
+
+        // ------------ Канал связи -------------------
+        // Алиса передаёт Бобу
+        val parentUuid1 = prevBlock.uuid
+        val bnid = prevBlock.bnid
+
+        // ----------- Сторона: Боб -------------------
+        // Верификация Боб-а
+
+        assert {
+            val h = Crypto.hash(walletAlice.sok.getString())
+            Crypto.verifySignature(h, walletAlice.sokSignature, BOK)
+        }
+
+        assert {
+            val h = getTransactionHash(parentUuid1, None, prevBlock.otok, bnid)
+            Crypto.verifySignature(h, aliceInitSignature, walletAlice.sok)
+        }
+
+        // Боб создаёт новый блок
+        val (uuid, parent_uuid, otok, _, init_wallet_signature) = walletBob.newBlockParams(
+            bnid = bnid,
+            parentUuid = parentUuid1
+        )
+
+        // ----------- Канал связи --------------------
+        // Передача данных от Боба Алисе через канал связи: uuid, parent_uuid, otok, sok, sok_signature_, init_wallet_signature
+        val sokBob = walletBob.sok
+        val sokSignatureBob = walletBob.sokSignature
+
+        // ---------- Сторона: Алиса -------------------
+        // Верификация
+
+        assert {
+            val h = Crypto.hash(sokBob.getString())
+            Crypto.verifySignature(h, sokSignatureBob, BOK)
+        }
+
+
+        assert {
+            val h = getTransactionHash(uuid, parent_uuid, otok, bnid)
+            Crypto.verifySignature(h, init_wallet_signature, sokBob)
+        }
+
+        // Алиса подписывает
+        val (magic, subscribe_transaction_hash, subscribe_transaction_signature) =
+            walletAlice.subscribe(uuid, parent_uuid!!, bnid)
+
+        // Проверим, что мы не можем один и тот же parent_uuid подписать дважды
+        assertThrow("Мы можем сделать 'штаны' в блокчейне!") { walletAlice.subscribe(uuid, parent_uuid, bnid) }
+
+        // ----------- Канал связи --------------------
+        // Алиса передаёт Бобу: magic, subscribe_transaction_hash, subscribe_transaction_signature
+
+        // ---------- Сторона: Боб -------------------
+        // Боб записывает блок
+
+        return OneBlock(
+            uuid = uuid,
+            parentUuid = parent_uuid,
+            bnid = bnid,
+            otok = otok,
+            magic = magic,
+            subscribeTransactionHash = subscribe_transaction_hash,
+            subscribeTransactionSignature = subscribe_transaction_signature,
         )
     }
 
